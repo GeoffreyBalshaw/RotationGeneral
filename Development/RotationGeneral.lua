49c5385e11482c89770ec049858cc6d8de773f10
Geoffrey Balshaw <geoffreybalshaw@gmail.com>
2019-02-05 16:38:42 -0500

    First commit to establish file structure

---
+++ Development/RotationGeneral.lua
@@ -0,0 +1,5 @@
+-- RotationGeneral
+-- Addon for World of Warcraft 3.3.5
+-- Written by Geoffrey Balshaw
+
+local RotationGeneral = LibStub( "AceAddon-3.0" ):NewAddon( "RotationGeneral", "AceConsole-3.0", "AceEvent-3.0" );
